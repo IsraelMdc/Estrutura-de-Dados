a24558aa0eab6f32d607dc2cc6557fce957b5aee
Palestina <israelmcarmo@hotmail.com>
2023-10-24 21:22:19 -0300

    MEu código em C

---
+++ Trabalho1/image.h
@@ -0,0 +1,20 @@
+/*
+ 
+
+//Creates a new image.
+
+Image* create(int rows, int cols, char type[]);
+
+//Loads an image from a PPM file.
+
+Image* load_from_ppm(const char* filename);
+
+//Writes an image to a PPM file.
+
+void write_to_ppm(Image* image, const char* filename);
+
+//Converts an RGB image to grayscale.
+
+void rgb_to_gray(Image* image_rgb, Image* image_gray);
+
+*/
